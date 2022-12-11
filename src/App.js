789e4de21f109bfc4f807be0cc5b09d31a9b2c2f
Aysel Ünlü <aysell.unlu448@gmail.com>
2022-12-12 01:07:04 +0300

    add blog page and browser routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import React from "react";
 import Header from "./Layout/Header";
 import Welcome from "./Pages/Welcome";
-
+import BlogPage from "./Pages/BlogPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div className="container-xl">
+    <div className="container">
       <Header />
-      <Welcome />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Welcome />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="home" element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
