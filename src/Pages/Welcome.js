0dbb31f5953f4115efb6aa87646cf48a45d83594
Aysel Ünlü <aysell.unlu448@gmail.com>
2022-12-11 20:32:05 +0300

    edit welcome

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -55,6 +55,53 @@ function Welcome() {
           </div>
         </div>
       </div>
+      <div className=" mb-5 pb-5 row">
+        <div className="green-box col">
+          <div className="orange-box text-left mb-4">
+            <h5 className="box-title">12 BURÇ</h5>
+            <p className="box-text">
+              Astroloji uzun süre astronomiyle aynı bilim dalı sayılmıştır.
+              Fakat astrolojinin kökeni astronomiye göre çok eskilere dayanır,
+              yaklaşık 6 bin yıl öncesine. Babil, Mısır, Yunan, Roma, Çin, Maya
+              ve Arap uygarlıklarının astroloji ile ilgilendiklerine dair pek
+              çok kanıt vardır.
+            </p>
+          </div>
+          <div className="col box-image text-center">
+            <Image src={AstroMap} height="250" />
+          </div>
+        </div>
+        <div className="green-box col">
+          <div className="orange-box text-left mb-4">
+            <h5 className="box-title">12 BURÇ</h5>
+            <p className="box-text">
+              Astroloji uzun süre astronomiyle aynı bilim dalı sayılmıştır.
+              Fakat astrolojinin kökeni astronomiye göre çok eskilere dayanır,
+              yaklaşık 6 bin yıl öncesine. Babil, Mısır, Yunan, Roma, Çin, Maya
+              ve Arap uygarlıklarının astroloji ile ilgilendiklerine dair pek
+              çok kanıt vardır.
+            </p>
+          </div>
+          <div className="col box-image text-center">
+            <Image src={AstroMap} height="250" />
+          </div>
+        </div>
+        <div className="green-box col">
+          <div className="orange-box text-left mb-4">
+            <h5 className="box-title">12 BURÇ</h5>
+            <p className="box-text">
+              Astroloji uzun süre astronomiyle aynı bilim dalı sayılmıştır.
+              Fakat astrolojinin kökeni astronomiye göre çok eskilere dayanır,
+              yaklaşık 6 bin yıl öncesine. Babil, Mısır, Yunan, Roma, Çin, Maya
+              ve Arap uygarlıklarının astroloji ile ilgilendiklerine dair pek
+              çok kanıt vardır.
+            </p>
+          </div>
+          <div className="col box-image text-center">
+            <Image src={AstroMap} height="250" />
+          </div>
+        </div>
+      </div>
       <div className="information-box row justify-content-center">
         <div className="blue-box col-6 p-5">
           <div className="first-purple-box">
